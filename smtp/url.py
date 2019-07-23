aaebcd00d6a625c3deab551e19348fcfe0257cd7
efralejandro <efralejandros@gmail.com>
2019-07-23 15:41:12 -0400

    [SMTP] updated url rep checking

--- smtp/url.py
+++ smtp/url.py
@@ -3,11 +3,12 @@ import json
 import re
 from urllib.parse import urlparse
 
+# Returns list with url's in a text by using regular expression matching.
 def get_urls(msg):
-    print(msg)
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
     return urls
 
+# Returns reputationScore of a certain domain based on its score at whoisxmlapi.com
 def reputation(domain):
     try:
         conn = http.client.HTTPSConnection("domain-reputation-api.whoisxmlapi.com")
@@ -18,5 +19,13 @@ def reputation(domain):
             j = json.loads(str)
         return int(j['reputationScore'])
     finally:
-        print("closing")
         conn.close()
+
+# Returns a list containing all low-rep domains contained in a text.
+def raise_rep(text):
+    urls = get_urls(text)
+    lowrep = []
+    for url in urls:
+        if reputation(url) < 60:
+            lowrep.append(url)
+    return lowrep
